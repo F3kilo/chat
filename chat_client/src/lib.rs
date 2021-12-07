61f24a6a5379295268b833493e59f90b16e1ddba
KirFed <f3lkilo@yandex.ru>
2021-12-07 13:19:32 +0300

    Basic client realization.

--- chat_client/src/lib.rs
+++ chat_client/src/lib.rs
@@ -1,3 +1,33 @@
+use std::net::ToSocketAddrs;
+use stp::client::{RequestResult, StpClient};
+use stp::error::ConnectResult;
+
+pub struct ChatClient {
+    stp: StpClient,
+}
+
+impl ChatClient {
+    pub fn new<Addr: ToSocketAddrs>(addr: Addr) -> ConnectResult<Self> {
+        let stp = StpClient::connect(addr)?;
+        Ok(Self { stp })
+    }
+
+    pub fn fetch(&mut self, room_id: &str) -> RequestResult {
+        let request = format!("fetch|||{}", room_id);
+        self.stp.send_request(request)
+    }
+
+    pub fn create_room(&mut self, room_id: &str) -> RequestResult {
+        let request = format!("create|||{}", room_id);
+        self.stp.send_request(request)
+    }
+
+    pub fn append(&mut self, room_id: &str, msg: &str) -> RequestResult {
+        let request = format!("append|||{}|||{}", room_id, msg);
+        self.stp.send_request(request)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
